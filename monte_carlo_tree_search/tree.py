be2a1783a1eb78c4d68a8e21908f4e00d23f9602
heemangpark <heemang0826@kaist.ac.kr>
2022-09-20 21:08:29 +0900

    temp

--- monte_carlo_tree_search/tree.py
+++ monte_carlo_tree_search/tree.py
@@ -12,20 +12,6 @@ class Tree:
         self.action_sequence = []
         _ = expand(1, self.g)
 
-    # def grow(self):
-    #     node_idx = 1
-    #     while self.g.nodes[node_idx]['state'] != TO:
-    #         prev_state = self.g.nodes[node_idx]['state']
-    #         node_idx, action = select(node_idx, self.g)
-    #         self.action_sequence.append(action)
-    #         next_state = transition(prev_state, action)
-    #         self.maze = State(next_state)
-    #         # print("took action \"{}\" from {} & now at {}".format(action, prev_state, next_state))
-    #         leaves = expand(node_idx, self.g)
-    #         for leaf in leaves:
-    #             sr = reward(random_rollout(self.g.nodes[leaf]['state'], 10))
-    #             backup(leaf, self.g, sr)
-
     def grow(self):
         while True:
             idx, self.action_sequence = 1, []
@@ -46,3 +32,18 @@ class Tree:
 
     def route(self):
         return self.action_sequence
+
+
+# def grow(self):
+#     node_idx = 1
+#     while self.g.nodes[node_idx]['state'] != TO:
+#         prev_state = self.g.nodes[node_idx]['state']
+#         node_idx, action = select(node_idx, self.g)
+#         self.action_sequence.append(action)
+#         next_state = transition(prev_state, action)
+#         self.maze = State(next_state)
+#         # print("took action \"{}\" from {} & now at {}".format(action, prev_state, next_state))
+#         leaves = expand(node_idx, self.g)
+#         for leaf in leaves:
+#             sr = simulated_reward(self.g.nodes[leaf]['state'], 10)
+#             backup(leaf, self.g, sr)
