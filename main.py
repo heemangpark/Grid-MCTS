eed54ff3e088a6fce03359addba36345182a7d18
heemangpark <heemang0826@kaist.ac.kr>
2022-09-20 20:58:33 +0900

    func_reward

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from greedy_agent import *
+import pickle
+import time
+
 from monte_carlo_tree_search.tree import *
 
 if __name__ == "__main__":
-    tree = Tree()
-    tree.grow()
-    route = tree.route()
-    print(route)
-    print(len(route))
+    for trial in range(1, 11):
+        start = int(time.time())
+        tree = Tree()
+        tree.grow()
+        route = tree.route()
+        nx.write_gpickle(tree, "tree_{}_{}".format(trial, int(time.time()) - start))
+        with open('route_{}_{}.pkl'.format(trial, len(route)), 'wb') as f:
+            pickle.dump(route, f)
 
-    """greedy"""
-    greedy_agent = Greedy_Agent()
-    # greedy_agent.play(rounds=100)
-    # print(greedy_agent.show_values())
+    # """greedy"""
+    # greedy_agent = Greedy_Agent()
+    # # greedy_agent.play(rounds=100)
+    # # print(greedy_agent.show_values())
--- monte_carlo_tree_search/tree.py
+++ monte_carlo_tree_search/tree.py
@@ -7,24 +7,42 @@ from monte_carlo_tree_search.tree_functions import *
 class Tree:
     def __init__(self, agent_id=0):
         self.g = nx.DiGraph()
-        self.g.add_node(1, state=FROM[agent_id], N=0)
+        self.g.add_node(1, state=FROM[agent_id], N=1)
         self.maze = State()
         self.action_sequence = []
+        _ = expand(1, self.g)
+
+    # def grow(self):
+    #     node_idx = 1
+    #     while self.g.nodes[node_idx]['state'] != TO:
+    #         prev_state = self.g.nodes[node_idx]['state']
+    #         node_idx, action = select(node_idx, self.g)
+    #         self.action_sequence.append(action)
+    #         next_state = transition(prev_state, action)
+    #         self.maze = State(next_state)
+    #         # print("took action \"{}\" from {} & now at {}".format(action, prev_state, next_state))
+    #         leaves = expand(node_idx, self.g)
+    #         for leaf in leaves:
+    #             sr = reward(random_rollout(self.g.nodes[leaf]['state'], 10))
+    #             backup(leaf, self.g, sr)
 
     def grow(self):
-        node_idx = 1
-        _ = expand(node_idx, self.g)
-        while self.g.nodes[node_idx]['state'] != TO:
-            prev_state = self.g.nodes[node_idx]['state']
-            node_idx, action = select(node_idx, self.g)
-            self.action_sequence.append(action)
-            next_state = transition(prev_state, action)
-            self.maze = State(next_state)
-            # print("took action \"{}\" from {} & now at {}".format(action, prev_state, next_state))
-            leaves = expand(node_idx, self.g)
+        while True:
+            idx, self.action_sequence = 1, []
+            """selection"""
+            while len(children(idx, self.g)) != 0:
+                idx, a = select(idx, self.g)
+                self.action_sequence.append(a)
+            """terminal check on selected leaf"""
+            if self.g.nodes[idx]['state'] == TO:
+                break
+            else:
+                pass
+            """expansion"""
+            leaves = expand(idx, self.g)
+            """backup"""
             for leaf in leaves:
-                sr = reward(random_rollout(self.g.nodes[leaf]['state'], 10))
-                backup(leaf, self.g, sr)
+                backup(leaf, self.g)
 
     def route(self):
         return self.action_sequence
